608c33be69c965125d82c144897f1f1d2dcefef1
Danny Cho <danheecho@gmail.com>
2022-08-09 00:50:55 -0700

    Registration back end WIP. Added a few sample cover photo images to assets.
    Bcrypt not set up. Validations not set up.

--- myspice_app/controllers/routes_controller.py
+++ myspice_app/controllers/routes_controller.py
@@ -1,4 +1,5 @@
 from myspice_app import app
+from myspice_app.models.user_model import Users
 from flask import render_template, redirect, session, request
 
 @app.route('/')
@@ -9,9 +10,24 @@ def login():
 def register(): 
     return render_template('registration.html')
 
+@app.route('/register', methods=['POST'])
+def register_post(): 
+    new_user = {
+        'first_name': request.form['registration_first_name'], 
+        'last_name': request.form['registration_last_name'], 
+        'email': request.form['registration_email'], 
+        'password': request.form['registration_password']
+    }
+    Users.register_user(new_user)
+    return redirect('/dashboard')
+
 @app.route('/dashboard')
 def dashboard(): 
     return render_template('dashboard.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
 
 @app.route('/profile/edit')
 def edit_profile(): 
@@ -28,7 +44,3 @@ def new_post():
 @app.route('/posts/1')
 def view_post(): 
     return render_template('view_post.html')
-
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from myspice_app import app
 from myspice_app.controllers import routes_controller
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
